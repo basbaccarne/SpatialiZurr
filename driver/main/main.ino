6fe0cf3f36898445cc5e8788b5c4caea0d75ab36
Bas Baccarne <bastiaan.baccarne@ugent.be>
2023-01-31 19:20:50 +0100

    read a message to a variable works

--- driver/main/main.ino
+++ driver/main/main.ino
@@ -2,37 +2,32 @@
 #include <ArduinoMqttClient.h>
 #include <WiFiNINA.h>
 
-// set passwords for shiftr & SSID
+// set passwords for WiFi & MQTT server (set passwords in seperate "arduino_secrets.h" file
 #include "arduino_secrets.h"
 char ssid[] = SECRET_SSID;
 char pass[] = SECRET_PASS;
 char mqttUsername[] = SECRET_MQTT_USER;
 char mqttPass[] = SECRET_MQTT_PASS;
-char direction[] = "0";
 
-// set-up wifi client
+// define clients
 WiFiClient wifiClient;
 MqttClient mqttClient(wifiClient);
 
-// set-up MQTT server information en topic to subscribe to
+// set MQTT server information en topic definition
 const char broker[] = "spatializurr.cloud.shiftr.io";
 int        port     = 1883;
-const char topic[]  = "robotconrol";
+const char topic[]  = "robotcontrol";
 
-// set-up variables for the delay
-const long interval = 1000;
+// declare variable for the delay
 unsigned long previousMillis = 0;
 
-// function to print what was received from MQTT
-void messageReceived(String &topic, String &payload) {
-  Serial.println(topic + ": " + payload);
-}
+// declare variable to story the incoming payload
+String payload;
 
 void setup() {
-  // open serial connection
   Serial.begin(9600);
 
-  // connect to SSID
+  // connect to WiFi
   Serial.print("Attempting to connect to SSID: ");
   Serial.println(ssid);
   while (WiFi.begin(ssid, pass) != WL_CONNECTED) {
@@ -41,60 +36,53 @@ void setup() {
     delay(5000);
   }
 
-  // authenticate with MQTT username and password
+  // set MQTT username and password for authentication
   mqttClient.setUsernamePassword(mqttUsername, mqttPass);
 
   // connect to the MQTT server
-  Serial.print("Verbinden met de MQTT broker: ");
-  Serial.print(broker);
-  Serial.println(" ...");
-
-  // when MQTT stuff breaks ...
+  Serial.print("Attempting to connect to the MQTT broker: ");
+  Serial.println(broker);
   if (!mqttClient.connect(broker, port)) {
     Serial.print("MQTT connection failed! Error code = ");
     Serial.println(mqttClient.connectError());
     while (1);
   }
+  Serial.println("You're connected to the MQTT broker!");
 
-  // when MQTT stuff works ...
-  Serial.println("Verbonden met de MQTT broker! Hoera!");
-
-  // Subscribe to topic
-  mqttClient.onMessage(onMqttMessage);
+  // subscribe to the topic
   Serial.print("Subscribing to topic: ");
   Serial.println(topic);
   mqttClient.subscribe(topic); 
+
+  // set the message receive callback function
+  mqttClient.onMessage(onMqttMessage); 
 }
 
 void loop() {
-  // poll regularly to avoid being disconnected by the broker
+  // call poll() regularly to allow the library to send MQTT keep alives which
+  // avoids being disconnected by the broker
   mqttClient.poll();
 
-  // settings for delay
+  // delay
   unsigned long currentMillis = millis();
 }
 
-// function that runs when a message is received
+// function that triggers when a signal comes in
 void onMqttMessage(int messageSize) {
-  // when a message is received, print metadata to serial
-  Serial.print("Received a message with topic '");
+  // print out the topic and some message details
+  Serial.println("Received a message: ");
   Serial.print(mqttClient.messageTopic());
   Serial.print("', length ");
   Serial.print(messageSize);
   Serial.println(" bytes:");
 
-  // define character variable for MQTT capture
-  char robotcontrol[messageSize];
+  // read the incoming data and store it into the payload variable
+  String payloadString = "";
+  for (int i = 0; i < messageSize; i++) {
+    payloadString += (char)mqttClient.read();
+  }
+  payload = payloadString;
 
-  // get data and store in a 'robotcontrol' variable
-  while (mqttClient.available()) {
-    for (int i = 0; i < messageSize; i++) {
-      robotcontrol[i] = (char)mqttClient.read(); // Read the characters into an array
-      }
-    }
-    // print the robotcontrol variable
-    Serial.println(robotcontrol);
-
-    // parse the robot control variable
-    // to be done
+  // print the payload
+  Serial.println(payload);  
 }
