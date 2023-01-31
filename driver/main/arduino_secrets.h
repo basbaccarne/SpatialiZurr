36173d532cb6be4041faee3a5f205e1d1fd78d33
Bas Baccarne <bastiaan.baccarne@ugent.be>
2023-01-31 19:21:22 +0100

    shhht

--- driver/main/arduino_secrets.h
+++ driver/main/arduino_secrets.h
@@ -1,4 +1,4 @@
-#define SECRET_SSID "iVisitor"
-#define SECRET_PASS "WelcomeATimec"
-#define SECRET_MQTT_USER "spatializurr"
-#define SECRET_MQTT_PASS "fJWgFP2u8chjYKCB"
+#define SECRET_SSID "..."
+#define SECRET_PASS "..."
+#define SECRET_MQTT_USER "..."
+#define SECRET_MQTT_PASS "..."
